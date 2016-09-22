53bc7321e1a48b5ad050a169ae99991871896e19
callananan <davidcallanan@Davids-iMac.local>
2016-09-22 19:06:37 +0100

    Initial Commit

---
+++ js/cv.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $("#aboutmespinner input").click(function() {
+        $("#aboutmeinfo").slideToggle("slow", function() {
+
+        });
+    });
+    $("#detailsspinner input").click(function() {
+        $("#detailsinfo").slideToggle("slow", function() {
+
+        });
+    });
+    $("#socialspinner input").click(function() {
+        $("#socialinfo").slideToggle("slow", function() {
+
+        });
+    });
+    $("#educationspinner input").click(function() {
+        $("#educationinfo").slideToggle("slow", function() {
+
+        });
+    });
+    $("#employmentspinner input").click(function() {
+        $("#employmentinfo").slideToggle("slow", function() {
+
+        });
+    });
+    $("#designspinner input").click(function() {
+        $("#designinfo").slideToggle("slow", function() {
+
+        });
+    });
+    $("#devspinner input").click(function() {
+        $("#devinfo").slideToggle("slow", function() {
+
+        });
+    });
+    $("#referencesspinner input").click(function() {
+        $("#referencesinfo").slideToggle("slow", function() {
+
+        });
+    });
+});
